63f5f5f19a14bef598acd6839db3f9222ddeb23a
Daigo <ad.asdasda2014@yandex.ru>
2021-11-20 15:02:12 +0600

    Добавьте файлы проекта.

---
+++ RPP_task_03/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace RPP_task_03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load audio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 346);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 84);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(570, 261);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(477, 346);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(311, 84);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 27);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Load video";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 442);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ RPP_task_03/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using AxWMPLib;
+
+namespace RPP_task_03
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Интерфейс для реализации проигрывателя по шаблону "Адаптер"
+        /// </summary>
+        public interface IPlayer
+        {
+            /// <summary>
+            /// Метод реализующий загрузку файла
+            /// </summary>
+            void LoadFile();
+
+            /// <summary>
+            /// Метод реализующий выбор файла.
+            /// </summary>
+            void SelectFile();
+        }
+
+        /// <summary>
+        /// Класс реализующий аудио-проигрыватель.
+        /// </summary>
+        public class AudioPlayer : IPlayer
+        {
+            List<String> files, Paths;
+            ListBox listBox1;
+            AxWindowsMediaPlayer axWindowsMediaPlayer1;
+
+            /// <summary>
+            /// Конструктор класса AudioPlayer, инициализирующий его объекты.
+            /// </summary>
+            /// <param name="listBox">Список для отображения файлов</param>
+            /// <param name="axWindowsMediaPlayer">Объект отображающий проигрыватель</param>
+            public AudioPlayer(ListBox listBox, AxWindowsMediaPlayer axWindowsMediaPlayer)
+            {
+                listBox1 = listBox;
+                axWindowsMediaPlayer1 = axWindowsMediaPlayer;
+                files = new List<string>();
+                Paths = new List<string>();
+            }
+
+            /// <summary>
+            /// Метод реализующий загрузку аудио-файла.
+            /// </summary>
+            public void LoadFile()
+            {
+                listBox1.Items.Clear();
+
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                ofd.Filter = "MP3 Files|*.mp3";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    files.AddRange(ofd.SafeFileNames);
+                    Paths.AddRange(ofd.FileNames);
+
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        listBox1.Items.Add(files[i]);
+                    }
+                }
+
+            }
+            /// <summary>
+            /// Метод реализующий выбор аудио-файла из списка.
+            /// </summary>
+            public void SelectFile()
+            {
+                if (listBox1.SelectedIndex != -1)
+                    axWindowsMediaPlayer1.URL = Paths[listBox1.SelectedIndex];
+            }
+        }
+        
+        /// <summary>
+        /// Класс реализующий видео-проигрыватель.
+        /// </summary>
+        public class VideoPlayer : IPlayer
+        {
+            List<String> files, Paths;
+            ListBox listBox1;
+            AxWindowsMediaPlayer axWindowsMediaPlayer1;
+            /// <summary>
+            /// Конструктор класса VideoPlayer, инициализирующий его объекты.
+            /// </summary>
+            /// <param name="listBox"></param>
+            /// <param name="axWindowsMediaPlayer"></param>
+            public VideoPlayer(ListBox listBox, AxWindowsMediaPlayer axWindowsMediaPlayer)
+            {
+                listBox1 = listBox;
+                axWindowsMediaPlayer1 = axWindowsMediaPlayer;
+                files = new List<string>();
+                Paths = new List<string>();
+            }
+            /// <summary>
+            /// Метод реализующий загрузку видео-файла.
+            /// </summary>
+            public void LoadFile()
+            {
+                listBox1.Items.Clear();
+
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                ofd.Filter = "MKV Files|*.mkv";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    files.AddRange(ofd.SafeFileNames);
+                    Paths.AddRange(ofd.FileNames);
+
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        listBox1.Items.Add(files[i]);
+                    }
+                }
+
+            }
+            /// <summary>
+            /// Метод реализующий выбор видео-файла.
+            /// </summary>
+            public void SelectFile()
+            {
+                if (listBox1.SelectedIndex != -1)
+                    axWindowsMediaPlayer1.URL = Paths[listBox1.SelectedIndex];
+            }
+        }
+
+        IPlayer AP;
+        IPlayer VP;
+
+        /// <summary>
+        /// Класс реализующий работу с Windows Forms
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            AP = new AudioPlayer(listBox1, axWindowsMediaPlayer1);
+            VP = new VideoPlayer(listBox2, axWindowsMediaPlayer1);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AP.SelectFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AP.LoadFile();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VP.LoadFile();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VP.SelectFile();
+        }
+    }
+}
